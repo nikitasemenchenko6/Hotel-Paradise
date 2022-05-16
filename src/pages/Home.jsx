5c9dea2beff388ddaa02c9a8c25ff425ddc0ed40
nikitasemenchenko6 <nikitasemenchenko6@gmail.com>
2022-05-17 00:49:48 +0600

    implemented header component ui with select date

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Header from '../components/Header/Header'
 import Navbar from '../components/Navbar/Navbar'
 
 const Home = () => {
   return (
     <div>
         <Navbar/>
+        <Header/>
     </div>
   )
 }
