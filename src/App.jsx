400668fa516c56f09a3f9d728c0b3558099f5c42
nikitasemenchenko6 <nikitasemenchenko6@gmail.com>
2022-05-17 00:27:45 +0600

    implement ui of home page navbar and created essential ui directory only

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Home from './pages/Home'
+
+function App() {
+
+
+  return (
+    <>
+      <Home/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Navbar.css';
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="navContainer">
+        <span className="logo">Hotel Paradise</span>
+        <div className="navItems">
+          <button className="navButton">Register</button>
+          <button className="navButton">Login</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
