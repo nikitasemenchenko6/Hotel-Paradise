a614b120316689be523fc33d8285ac1ece086c98
nikitasemenchenko6 <nikitasemenchenko6@gmail.com>
2022-05-28 23:32:37 +0600

    implemented featured propeties ui

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Featured from '../../components/Featured/Featured'
+import FeaturedProperties from '../../components/FeaturedProperties/FeaturedProperties';
 import Header from '../../components/Header/Header'
 import Navbar from '../../components/Navbar/Navbar'
 import PropertyList from '../../components/PropertyList/PropertyList';
@@ -14,6 +15,8 @@ const Home = () => {
           <Featured/>
           <h1 className="homeTitle">Browse by property type</h1>
           <PropertyList/>
+          <h1 className="homeTitle">Homes guests love</h1>
+          <FeaturedProperties/>
         </div>
     </div>
   )
