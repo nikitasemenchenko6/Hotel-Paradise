5f34fd1afd0f2b4d1df798f791d94a09d9bef137
nikitasemenchenko6 <nikitasemenchenko6@gmail.com>
2022-07-31 00:14:45 +0600

    connected with mongodb

--- api/index.js
+++ api/index.js
@@ -1,7 +1,27 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
+//middlware
 const app = express();
+dotenv.config();
+
+const connect = async () =>{
+    try{
+        mongoose.connect(process.env.MONGO);
+        console.log("Connected with mongodb")
+    }
+    catch(error){
+        throw error;
+    }
+}
+
+mongoose.connection.on("disconnected", ()=>{
+    console.log("disconnected");
+})
+
 
 app.listen(8000, () =>{
+    connect();
     console.log("connected")
 })
